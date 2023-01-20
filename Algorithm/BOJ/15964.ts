b61ffc67bfafef9faf2352cf495ade666b82bce5
steve <siriyay@naver.com>
2023-01-21 03:35:43 +0900

    15964

---
+++ Algorithm/BOJ/15964.ts
@@ -0,0 +1,14 @@
+// 언어: nodejs
+// 메모리: 9340 KB
+// 시간: 120 ms
+// 코드길이: 209 B
+
+
+const input = require('fs').readFileSync(0).toString().split(' ').map(Number);
+
+const test = (a,b) => {
+    let result = a*a-b*b;
+    return result
+}
+
+console.log(test(parseInt(input[0]),parseInt(input[1])));
