38a1a9503005a408814146b4b1980e3ed5821e86
steve <siriyay@naver.com>
2023-01-21 23:16:21 +0900

    0121

---
+++ Dynamic Programming/Seed/2475.ts
@@ -0,0 +1,15 @@
+// 언어: nodejs
+// 메모리: 9332 KB
+// 시간: 120 ms
+// 코드길이: 206 B
+
+const input = require('fs').readFileSync(0).toString().split(' ').map(Number);
+
+const func = (n) =>{
+    return n*n;
+}
+let result = 0;
+for(const n of input){
+    result += func(n);
+}
+console.log(result%10);
