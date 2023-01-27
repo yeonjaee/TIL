119f333fd0118abce98b2884d7d41b5a7dc939ae
steve <siriyay@naver.com>
2023-01-28 00:43:56 +0900

    1920

---
+++ Dynamic Programming/2 class/1920.ts
@@ -0,0 +1,34 @@
+// 이진 탐색 알고리즘 사용하는 문제. - 데이터가 정렬돼 있는 배열에서 특정한 값을 찾아내는 알고리즘
+
+// @ts-ignore
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+
+const f = input[1].split(" ").map((x) => parseInt(x));
+const s = input[3].split(" ").map((x) => parseInt(x));
+// 먼저 정렬
+f.sort((a, b) => a - b);
+
+let a = [];
+s.forEach((e) => {
+  let low = 0;
+  let high = f.length - 1;
+  let result = false;
+
+  while (low <= high) {
+    let mid = (low + high) / 2;
+    if (f[mid] === e) {
+      result = true;
+      break;
+    } else if (f[mid] > e) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+    return -1;
+  }
+
+  if (result) a.push(1);
+  else a.push(0);
+});
+
+console.log(a.join("\n"));
