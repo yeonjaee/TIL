e9a689b5b1399ded5e985755874e35f80ef373e4
steve <siriyay@naver.com>
2023-02-03 00:11:54 +0900

    10773

---
+++ Dynamic Programming/2 class/10773.ts
@@ -0,0 +1,23 @@
+// 언어: nodejs
+// 메모리: 17528 KB
+// 시간: 256 ms
+// 코드길이: 328 B
+
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const numbs = [];
+
+for (let i = 1; i <= n; i++) {
+  let num = Number(input[i]);
+  if (num === 0) numbs.pop();
+  else numbs.push(num);
+}
+
+let result = 0;
+
+for (let num2 of numbs) {
+  result += num2;
+}
+
+console.log(result);
