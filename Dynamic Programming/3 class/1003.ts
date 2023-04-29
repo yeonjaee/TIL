c6b7d12a20086524606e86f778673c193432a546
steve <siriyay@naver.com>
2023-04-29 19:09:16 +0900

    1003

---
+++ Dynamic Programming/3 class/1003.ts
@@ -0,0 +1,19 @@
+// 언어: nodejs
+// 메모리: 9328 KB
+// 시간: 124 ms
+// 쾨드길이: 209 B
+
+// @ts-ignore
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+const len = input.shift();
+for (let i = 0; i < len; i++) {
+    const n = input[i];
+    const fibonacci = [[1, 0], [0, 1]];
+    for (let j = 2; j <= n; j++) {
+        fibonacci[j] = [
+            fibonacci[j-1][0] + fibonacci[j-2][0],
+            fibonacci[j-1][1] + fibonacci[j-2][1]
+        ];
+    }
+    console.log(fibonacci[n].join(" "));
+}
